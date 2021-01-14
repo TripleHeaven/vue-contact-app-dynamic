73286e5afda7a0262d740859e9a9cb67513d9888
Ilya <heaven30.buroff@gmail.com>
2021-01-14 15:03:18 +0300

    changed router

--- src/router.js
+++ src/router.js
@@ -11,6 +11,7 @@ Vue.use(Router);
 
 export default new Router ({
   mode: 'history',
+  base: process.env.VUE_APP_MODE === 'production' ? '/' : '/contacts-app/',
   routes : [
     {
     path: '/',
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath : '/contacts-app/'
+  baseUrl: process.env.VUE_APP_MODE === 'production' ? '/contacts-app/' : '/',
 }
